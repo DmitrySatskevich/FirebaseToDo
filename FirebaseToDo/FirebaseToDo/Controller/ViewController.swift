a96dddb792b1c5f8a566131e184d01d0beda0308
Dmitry Satskevich <Satsckevich1993@yandex.ru>
2023-01-28 21:27:37 +0300

    saving the user to the database

--- FirebaseToDo/FirebaseToDo/Controller/ViewController.swift
+++ FirebaseToDo/FirebaseToDo/Controller/ViewController.swift
@@ -20,7 +20,32 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        emailTF.delegate = self
+        passwordTF.delegate = self
+        ref = Database.database().reference(withPath: "users")
+
+        // если у нас еще есть действующий user то сделаем переход
+//        authStateDidChangeListenerHandle = Auth.auth().addStateDidChangeListener { [weak self] _, user in
+//            guard let _ = user else {
+//                return
+//            }
+//            self?.performSegue(withIdentifier: "tasksSegue", sender: nil)
+//        }
+
+        NotificationCenter.default.addObserver(self, selector: #selector(kbDidShow), name: UIWindow.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(kbDidHide), name: UIWindow.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // чистим поля
+        emailTF.text = ""
+        passwordTF.text = ""
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self)
+//        Auth.auth().removeStateDidChangeListener(authStateDidChangeListenerHandle)
     }
 
     @IBAction func signInAction() {
@@ -68,17 +93,44 @@ class LoginViewController: UIViewController {
     
     // анимация появления Error
     private func displayErrorLabel(withText text: String) {
-       errorLabel.text = text
-       UIView.animate(withDuration: 5,
-                      delay: 0,
-                      usingSpringWithDamping: 1,
-                      initialSpringVelocity: 1,
-                      options: .curveEaseInOut, // плавно появляется и плавно исчезает
-                      animations: { [weak self] in
-                          self?.errorLabel.alpha = 1
-                      }) { [weak self] _ in
-           self?.errorLabel.alpha = 0
-       }
-   }
+        errorLabel.text = text
+        UIView.animate(withDuration: 5,
+                       delay: 0,
+                       usingSpringWithDamping: 1,
+                       initialSpringVelocity: 1,
+                       options: .curveEaseInOut, // плавно появляется и плавно исчезает
+                       animations: { [weak self] in
+                           self?.errorLabel.alpha = 1
+                       }) { [weak self] _ in
+            self?.errorLabel.alpha = 0
+        }
+    }
+    
+//  Смещение экрана при появлении клавиатуры
+    @objc func kbDidShow(notification: Notification) {
+        self.view.frame.origin.y = 0
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            self.view.frame.origin.y -= (keyboardSize.height / 2)
+        }
+    }
+    
+//  При закрытии клавиатура, экран возвращается в исходное положение
+    @objc func kbDidHide() {
+        self.view.frame.origin.y = 0
+    }
 }
 
+extension LoginViewController: UITextFieldDelegate {
+//    Скрываем клавиатуру, по нажатию "return".
+//    Text Field нужно подключить "delegate"
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    // Скрытие клавиатуры по тапу за пределами Text Field
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true) // Скрывает клавиатуру, вызванную для любого объекта
+    }
+}
